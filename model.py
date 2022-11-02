f5f75ac0d3df3a4e21372d82df004cff55f13f25
cilemafacan <afacan.cilem@gmail.com>
2022-11-02 10:55:12 +0300

    model changed

--- model.py
+++ model.py
@@ -7,18 +7,33 @@ class CNN(nn.Module):
         self.encoder = nn.Sequential(
                 nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1),
                 nn.PReLU(),
-                nn.AvgPool2d(kernel_size=2),
                 nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1),
                 nn.PReLU(),
                 nn.AvgPool2d(kernel_size=2),
                 nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1),
                 nn.PReLU(),
-                nn.AvgPool2d(kernel_size=2),
                 nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1),
                 nn.PReLU(),
+                nn.AvgPool2d(kernel_size=2),
+                nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
+                nn.PReLU(),
                 nn.AvgPool2d(kernel_size=2))
-        
+        self.flatten = nn.Flatten()
+        self.bottleneck = nn.Sequential(
+                nn.Linear(in_features=5*5*ch[4], out_features=2048),
+                nn.PReLU(),
+                nn.Linear(in_features=2048, out_features=1024),
+                nn.PReLU(),
+                nn.Linear(in_features=1024, out_features=2048),
+                nn.PReLU(),
+                nn.Linear(in_features=2048, out_features=5*5*ch[4]),
+                nn.PReLU()
+        )
+        #self.unflatten  = nn.Unflatten(dim=1, unflattened_size=(ch[4],5,5))
         self.decoder = nn.Sequential(
+                nn.Upsample(scale_factor=2),
+                nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=1),
+                nn.PReLU(),
                 nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=1),
                 nn.PReLU(),
                 nn.Upsample(scale_factor=2),
@@ -27,14 +42,13 @@ class CNN(nn.Module):
                 nn.Upsample(scale_factor=2),
                 nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=3, stride=1),
                 nn.PReLU(),
-                nn.Upsample(scale_factor=2),
-                nn.ConvTranspose2d(in_channels=16, out_channels=3, kernel_size=3, stride=1),
-                nn.PReLU(),
-                nn.Upsample(size=(64,64)))
+                nn.ConvTranspose2d(in_channels=16, out_channels=3, kernel_size=3, stride=1))
         
     def forward(self, x):
         encoder = self.encoder(x)
-        decoder = self.decoder(encoder)
+        flatten = self.flatten(encoder)
+        bottleneck = self.bottleneck(flatten).view(-1,256,5,5)
+        decoder = self.decoder(bottleneck)
         sigmoid    = nn.Sigmoid()
         output     = sigmoid(decoder)
 
